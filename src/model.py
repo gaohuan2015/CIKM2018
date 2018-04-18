6e3318ba44b2dad0b1a79c7248e23638a896eb6e
duanshangfu <sf_duan@seu.edu.cn>
2018-04-18 19:08:44 +0800

    forward

--- src/model.py
+++ src/model.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 
 
 class Encoder(nn.Module):
-    def __init__(self):
+    def __init__(self, word2vec):
         super(Encoder, self).__init__()
 
         # hyper-parameters
@@ -16,16 +16,18 @@ class Encoder(nn.Module):
         self.bag_size = 20
         self.windows_size = 3
         self.kernel_num = 64
-        self.vocabulary_size = 100
+        self.vocabulary_size = word2vec.shape[0]
         self.pos_embedding_size = 5
         self.relation_num = 99
+        self.max_sentence_size = 100
+        self.pos_size = 120
 
         # embedding layer
         self.embedding = nn.Embedding(self.vocabulary_size, self.word_embedding_size)
-        self.pos_embedding = nn.Embedding(120, self.pos_embedding_size)
+        self.pos_embedding = nn.Embedding(self.pos_size, self.pos_embedding_size)
         # self.position2_embedding = nn.Embedding(1, self.pos_embedding_size)
 
-        # self.embeddingd.weight.data.copy_(torch.from_numpy())
+        self.embedding.weight.data.copy_(torch.from_numpy(word2vec))
 
         # part1:sentence encoder layer
         self.sentence_encoder = nn.Sequential(
@@ -47,13 +49,15 @@ class Encoder(nn.Module):
             nn.ReLU()
         )
 
-    def forward(self, sentence, pos):
+    def forward(self, sentence, pos1, pos2):
         # 输入的bag 2D-tensor n_sample * sentence_length
 
-        word_embedding = self.embedding(sentence).view(-1, 1, self.sentence_length, self.word_embedding_size)
-        pos1_embedding = self.pos_embedding(pos).view(-1, 1, 1, 2 * self.pos_embedding_size)
+        word_embedding = self.embedding(sentence).view(-1, 1, self.max_sentence_size, self.word_embedding_size)
+        pos1_embedding = self.pos_embedding(pos1).view(-1, 1, self.max_sentence_size, self.pos_embedding_size)
+        pos2_embedding = self.pos_embedding(pos2).view(-1, 1, self.max_sentence_size, self.pos_embedding_size)
 
-        input_embedding = torch.cat(word_embedding, pos1_embedding, -1)
+
+        input_embedding = torch.cat((word_embedding, pos1_embedding, pos2_embedding), -1)
 
         H = self.sentence_encoder(input_embedding).view(-1, self.kernel_num)
 
